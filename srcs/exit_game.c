b1278d6d03e44c7f298e127da10f59dc288be1b4
Victorian Roques <victorianroques@Victorians-MacBook-Pro.local>
2020-10-26 22:59:20 +0100

    add minilib linux

--- srcs/exit_game.c
+++ srcs/exit_game.c
@@ -1,8 +1,29 @@
 #include "../include/cub3d.h"
 
+static void		free_map_world_and_text(t_mlx *mlx)
+{
+	int i;
+
+	i = 0;
+	while (i < mlx->map.line)
+	{
+		free(mlx->map.world_map[i]);
+		i++;
+	}
+	free(mlx->map.world_map);
+	i = 0;
+	while (i < 5)
+	{
+		free(mlx->texture[i]);
+		i++;
+	}
+	free(mlx->texture);
+}
+
 int		exit_game(t_mlx *mlx)
 {
 	mlx_destroy_window(mlx->mlx_ptr, mlx->win);
+	free_map_world_and_text(mlx);
 	exit(0);
 }
 
